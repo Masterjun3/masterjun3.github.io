52fc12b13ddb6857b4051a350a2580a6db2e9b10
Masterjun <masterjun3@gmail.com>
2017-12-07 21:43:59 +0100

    DeSmuME

---
+++ desmume.js
@@ -0,0 +1,9 @@
+var s = "desmume";
+var sn;
+do {
+  sn="";
+  for (var i=0; i<s.length; i++){
+    sn = sn + (Math.random()<0.5 ? s.charAt(i).toUpperCase() : s.charAt(i).toLowerCase());
+  }
+} while (sn=="DeSmuME" || sn=="desmume" || sn=="DESMUME");
+document.getElementById("wrong").innerHTML = "not "+sn;
