50630216d2ba451906695c744d33d1f1247a1177
Masterjun <masterjun3@gmail.com>
2017-12-07 23:11:50 +0100

    reroll

--- desmume.js
+++ desmume.js
@@ -1,9 +1,13 @@
-var s = "desmume";
-var sn;
-do {
-  sn="";
-  for (var i=0; i<s.length; i++){
-    sn = sn + (Math.random()<0.5 ? s.charAt(i).toUpperCase() : s.charAt(i).toLowerCase());
-  }
-} while (sn=="DeSmuME" || sn=="desmume" || sn=="DESMUME");
-document.getElementById("wrong").innerHTML = "not "+sn;
+function reroll(){
+  var s = "desmume";
+  var sn;
+  do {
+    sn="";
+    for (var i=0; i<s.length; i++){
+      sn = sn + (Math.random()<0.5 ? s.charAt(i).toUpperCase() : s.charAt(i).toLowerCase());
+    }
+  } while (sn=="DeSmuME" || sn=="desmume" || sn=="DESMUME");
+  document.getElementById("wrong").innerHTML = "not "+sn;
+}
+reroll();
+setInterval(reroll,100)
